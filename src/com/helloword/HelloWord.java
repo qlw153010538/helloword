21c2659d6b9eadf3b13e66ccd1917e5f9b740a83
qiliwen <1084556884@qq.com>
2020-06-17 17:39:27 +0800

    添加DoubleUtil方法

--- src/com/helloword/HelloWord.java
+++ src/com/helloword/HelloWord.java
@@ -3,5 +3,12 @@ package com.helloword;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word!!!");
+        DoubleUtil();
+    }
+
+    public static double DoubleUtil(){
+
+        double number=1;
+        return number;
     }
 }
