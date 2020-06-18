19344a66c8af8fbb28c98c4654f11be44183976c
qiliwen <1084556884@qq.com>
2020-06-18 15:58:40 +0800

    添加实时代码模板

--- src/com/helloword/HelloWord.java
+++ src/com/helloword/HelloWord.java
@@ -4,6 +4,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word!!!");
         System.out.println("修改一个BUG!!!");
+        System.out.println("添加实时代码模板");
         System.out.println("-----------Live Templates实时代码模板测试域名YouMeek.com-----------11值=" + 11 + "," + "当前类=HelloWord.main()");
     }
 
