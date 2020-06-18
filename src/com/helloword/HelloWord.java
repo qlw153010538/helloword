c738d3a045a7c3dd1511e8d425c3e4a24c460dc4
qiliwen <1084556884@qq.com>
2020-06-18 12:31:17 +0800

    修改完BUG

--- src/com/helloword/HelloWord.java
+++ src/com/helloword/HelloWord.java
@@ -3,8 +3,8 @@ package com.helloword;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word!!!");
-
-        System.out.println("没有BUG!!!");
+        System.out.println("修改一个BUG!!!");
+        System.out.println("-----------Live Templates实时代码模板测试域名YouMeek.com-----------11值=" + 11 + "," + "当前类=HelloWord.main()");
     }
 
 }
